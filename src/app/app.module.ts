fabba1d61283df6fac4c3d8b019c2ee4bb1d59ec
Gabriel Falcones <gafalcon@espol.edu.ec>
2019-09-18 17:37:48 -0400

    reactive forms for song upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
 import { PlaylistComponent } from './playlist/playlist.component';
@@ -10,6 +13,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SimpleNotificationsModule } from 'angular2-notifications';
 import { NewSongComponent } from './new-song/new-song.component';
+
 
 @NgModule({
   declarations: [
@@ -25,6 +29,8 @@ import { NewSongComponent } from './new-song/new-song.component';
       BrowserModule,
       AppRoutingModule,
       BrowserAnimationsModule,
+      FormsModule,
+      ReactiveFormsModule,
       SimpleNotificationsModule.forRoot({
           position: ['bottom', 'right'],
           timeOut: 5000,
--- src/app/new-song/new-song.component.ts
+++ src/app/new-song/new-song.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-new-song',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewSongComponent implements OnInit {
 
-  constructor() { }
+    // name = new FormControl('');
 
-  ngOnInit() {
-  }
+    genres = [
+        { id: 1, name: 'rock' },
+        { id: 2, name: 'pop' },
+        { id: 3, name: 'classical' },
+        { id: 4, name: 'r&b' }
+    ];
+
+    mediaFile: File;
+
+    form: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+        this.form = this.formBuilder.group({
+            name: [''],
+            genres: this.formBuilder.array([]),
+            album: [''],
+            media_file: [''],
+            lyrics: ['']
+        });
+
+        this.addGenreCheckboxes();
+    }
+    private addGenreCheckboxes() {
+        this.genres.forEach((o, i) => {
+            const control = new FormControl(i === 0); // if first item set to true, else false
+            (this.form.controls.genres as FormArray).push(control);
+        });
+    }
+
+    ngOnInit() {
+    }
+
+    onSubmit() {
+        console.warn(this.form.value);
+    }
+
+    fileUpload(evt){
+        console.log(evt.target.files);
+        this.mediaFile = evt.target.files[0];
+    }
 
 }
